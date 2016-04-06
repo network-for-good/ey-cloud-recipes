35ba56496df511bbced70e46b27b434899371220
Timothy King <tmk@lordzork.com>
2016-04-06 11:08:23 -0400

    Modifications to logentries config

--- cookbooks/le/recipes/configure.rb
+++ cookbooks/le/recipes/configure.rb
@@ -13,10 +13,6 @@ end
 
 follow_paths = [
   "/var/log/syslog",
-  "/var/log/auth.log",
-  "/var/log/daemon.log",
-  "/var/log/chef.custom.log",
-  "/var/log/chef.main.log"
 ]
 
 case node[:instance_role]
@@ -26,18 +22,12 @@ when 'app', 'app_master', 'solo'
   (node[:applications] || []).each do |app_name, app_info|
     follow_paths << "/var/log/nginx/#{app_name}.access.log"
     follow_paths << "/var/log/nginx/#{app_name}.error.log"
-    follow_paths << "/var/log/engineyard/apps/#{app_name}/oink.log"
-    follow_paths << "/var/log/engineyard/apps/#{app_name}/production.log"
-    follow_paths << "/var/log/engineyard/apps/#{app_name}/production_cron_tasks.log"
-    follow_paths << "/var/log/engineyard/apps/#{app_name}/#{env}.log" unless env == 'production'
+    follow_paths << "/var/log/engineyard/apps/#{app_name}/#{env}.log"
   end
 when 'util'
   (node[:applications] || []).each do |app_name, app_info|
-    %w(0 1 2 3 scheduler).each do |log_suffix|
-      follow_paths << "/var/log/engineyard/apps/#{app_name}/resque_#{log_suffix}.log"
-    end
+    follow_paths << "/var/log/engineyard/apps/#{app_name}/#{env}.log"
   end
-  ["/data/redis/redis.log"].each { |file| follow_paths << file }
 when 'db_master', 'db_slave'
   [
     '/db/mysql/5.5/log/mysqld.err',
