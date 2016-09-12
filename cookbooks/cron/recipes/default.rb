1f4799ff3ddfe6df952d88e88ae12431ae3b0589
Timothy King <tmk@lordzork.com>
2016-09-12 11:05:26 -0400

    Adds custom cache cleanup script to cron.daily

--- cookbooks/cron/recipes/default.rb
+++ cookbooks/cron/recipes/default.rb
@@ -4,7 +4,8 @@
 #
 
 # Find all cron jobs specified in attributes/cron.rb where current node name matches instance_name
-crons = node[:custom_crons].find_all {|c| c[:instance_name] == "#{node[:name]}" }
+#crons = node[:custom_crons].find_all {|c| c[:instance_name] == "#{node[:name]}" }
+crons = {}
 
 crons.each do |cron|
   cron cron[:name] do
@@ -16,5 +17,18 @@ crons.each do |cron|
     month    cron[:time].split[3]
     weekday  cron[:time].split[4]
     command  cron[:command]
+  end
+end
+
+case node[:instance_role]
+when 'util', 'solo'
+  node[:applications].keys.each do |app_name|
+    template '/etc/cron.daily/clean_rails_cache_dir.sh' do
+      source 'clean_rails_cache_dir.sh.erb'
+      variables({
+        app_dir: "/data/#{app_name}/shared/tmp/cache"
+      })
+      mode '0755'
+    end
   end
 end
--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -54,7 +54,7 @@ include_recipe "collectd"
 
 #uncomment to add specified cron jobs for application user (deploy)
 # You must add your cron jobs to cron/attributes/cron.rb
-#require_recipe "cron"
+require_recipe "cron"
 
 #uncomment to run the exim::auth recipe
 #include_recipe "exim::auth"
