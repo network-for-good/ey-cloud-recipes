dd0c3ef376724d2f950749c76981979d30c38470
Timothy King <tmk@lordzork.com>
2017-07-25 13:00:51 -0400

    Refactors user check

--- cookbooks/postgresql_readonly_user/recipes/default.rb
+++ cookbooks/postgresql_readonly_user/recipes/default.rb
@@ -10,8 +10,6 @@ database_name = app['database_name']
 database_username = 'postgres'
 database_password = app['database_password']
 sql = <<-EOL
-DROP OWNED BY #{reporting_user};
-DROP ROLE IF EXISTS #{reporting_user};
 CREATE ROLE #{reporting_user} LOGIN PASSWORD '!0QpalKsow92';
 GRANT CONNECT ON DATABASE #{database_name} TO #{reporting_user};
 GRANT USAGE ON SCHEMA public TO #{reporting_user};
@@ -19,8 +17,11 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO #{reporting_user};
 EOL
 
 if node[:instance_role] == 'db_master'
-  execute "psql" do
-    command %Q{sudo -u #{database_username} /usr/bin/createuser -U #{database_username} -D -R -S #{reporting_user}}
-    command %Q{sudo -u #{database_username} psql -U #{database_username} -d #{database_name} -c "#{sql}"}
+  user_exists = `psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='#{reporting_user}'"`
+  unless user_exists.strip == 1.to_s
+    execute "psql" do
+      command %Q{sudo -u #{database_username} /usr/bin/createuser -U #{database_username} -D -R -S #{reporting_user}}
+      command %Q{sudo -u #{database_username} psql -U #{database_username} -d #{database_name} -c "#{sql}"}
+    end
   end
 end
